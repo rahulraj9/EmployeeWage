551be402aa9d464b9be6276ed8c235159409f8a0
Rahul Raj <jokingboyrahul@gmail.com>
2021-01-29 19:15:42 +0530

    [Rahul]SwitchCase SolvingUsingSwitchCase

--- EmpWage.java
+++ EmpWage.java
@@ -1,25 +1,25 @@
 public class  EmpWage{
+	public static final int IS_PART_TIME = 1;
+        public static final int IS_FULL_TIME = 2;
+        public static final int EMP_RATE_PERr_HRS = 20;
+	
 	public static void main(String [] args){
 		int empWage =0;
-		int isPartTime = 1;
-		int isFullTime = 2;
-		int empRatePerHrs = 20;
 		int empHrs =0;
-		double empPresent = Math.floor(Math.random() * 10) % 3;
-		if(empPresent == isPartTime){
-			System.out.println("Part Time Employee");
-			empHrs = 4;
-		}
-		else if(empPresent == isFullTime) {
-			System.out.println("Full Time Employee");
-			empHrs = 8;
-		}
-		else{
-			System.out.println("Employee is not a part of Organisation");
-		}		empWage = empHrs * empRatePerHrs;
+		int empPresent = (int)Math.floor(Math.random() * 10) % 3;
+		switch(empPresent){
+			case IS_PART_TIME:
+				System.out.println("Part Time Employee");
+				empHrs = 4;
+				break;
+			case IS_FULL_TIME:
+				System.out.println("Full Time Employee");
+				empHrs = 8;
+				break;
+			default:
+				System.out.println("Employee is Absent");
+		}		
+		empWage = empHrs * EMP_RATE_PERr_HRS;
 		System.out.println("Employee Wage:\t"+empWage);
 	}
 }
-
-
-
