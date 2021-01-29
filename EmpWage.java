337ddff761ec79152270fe0c624b5c536b5f07af
Rahul Raj <jokingboyrahul@gmail.com>
2021-01-29 19:19:45 +0530

    [Rahul]Calculate CalculatingWageForMonth

--- EmpWage.java
+++ EmpWage.java
@@ -1,25 +1,36 @@
 public class  EmpWage{
-	public static final int IS_PART_TIME = 1;
-        public static final int IS_FULL_TIME = 2;
-        public static final int EMP_RATE_PERr_HRS = 20;
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HRS = 20;
+    public static final int EMP_WORKING_DAYS = 2;
 	
 	public static void main(String [] args){
 		int empWage =0;
 		int empHrs =0;
-		int empPresent = (int)Math.floor(Math.random() * 10) % 3;
-		switch(empPresent){
-			case IS_PART_TIME:
-				System.out.println("Part Time Employee");
-				empHrs = 4;
-				break;
-			case IS_FULL_TIME:
-				System.out.println("Full Time Employee");
-				empHrs = 8;
-				break;
-			default:
-				System.out.println("Employee is Absent");
-		}		
-		empWage = empHrs * EMP_RATE_PERr_HRS;
-		System.out.println("Employee Wage:\t"+empWage);
+		int totalWage = 0;
+		for(int Days = 1; Days <= EMP_WORKING_DAYS; Days++){
+			 int empPresent = (int)Math.floor(Math.random() * 10) % 3;
+			
+			 switch(empPresent){
+				case IS_PART_TIME:
+					System.out.println("Part Time Work");
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					System.out.println("Full Time Work");
+					empHrs = 8;
+					break;
+				default:
+					empHrs = 0;
+					System.out.println("Employee is not Present");
+			}		
+			
+			empWage = empHrs * EMP_RATE_PER_HRS;
+			System.out.println("empWage for Days " +Days +":-" +empWage);
+			totalWage += empWage; 
+		}
+		
+		System.out.println("Employee totalWage:\t"+totalWage);
 	}
 }
+
