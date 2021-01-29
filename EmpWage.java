737b59c2b4490bf00073823184281565bca6e9ad
Rahul Raj <jokingboyrahul@gmail.com>
2021-01-29 19:02:03 +0530

    [Rahul]Add AddingPartTimeEmp

--- EmpWage.java
+++ EmpWage.java
@@ -1,22 +1,22 @@
 public class  EmpWage{
 	public static void main(String [] args){
 		int empWage =0;
-		int isPresent = 1;
+		int isPartTime = 1;
+		int isFullTime = 2;
 		int empRatePerHrs = 20;
 		int empHrs =0;
-		
-		double empPresent = Math.floor(Math.random() * 10) % 2;
-		
-		if(empPresent == isPresent){
-			System.out.println("Employee is Present");
+		double empPresent = Math.floor(Math.random() * 10) % 3;
+		if(empPresent == isPartTime){
+			System.out.println("Part Time Employee");
+			empHrs = 4;
+		}
+		else if(empPresent == isFullTime) {
+			System.out.println("Full Time Employee");
 			empHrs = 8;
 		}
 		else{
-			System.out.println("Employee is Absent");
-			empHrs = 0;
-		}
-		
-		empWage = empHrs * empRatePerHrs;
+			System.out.println("Employee is not a part of Organisation");
+		}		empWage = empHrs * empRatePerHrs;
 		System.out.println("Employee Wage:\t"+empWage);
 	}
 }
