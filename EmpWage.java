598edf9afe0c5342dca528413e6cc2f72f7c0794
Rahul Raj <jokingboyrahul@gmail.com>
2021-01-29 18:04:25 +0530

    [Rahul]Attendence CheckEmpIsPresentOrNot

--- EmpWage.java
+++ EmpWage.java
@@ -1,7 +1,11 @@
-public class EmpWage {
+public class EmpWage{
 	public static void main(String [] args){
-		System.out.println(":::::::Welcome To Employe Wage:::::::");
-		
-
+		int isPresent = 1;
+		double empCheck = Math.floor (Math.random() * 10 )% 2;
+		if(empCheck == isPresent)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
+
